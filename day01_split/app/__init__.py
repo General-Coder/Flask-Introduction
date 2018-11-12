511c4540e994a49594c19e6fe6378c0334e94f89
General-Iter <17625904460@163.com>
2018-11-12 20:14:51 +0800

    新增session持久化

--- day01_split/app/__init__.py
+++ day01_split/app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_session import Session
+
 from .views import *
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = 'guanfang'
+    app.config['SESSION_TYPE'] = 'redis'
+    Session(app=app)
     app.register_blueprint(blueprint=blue)
     return  app
